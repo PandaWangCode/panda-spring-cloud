65e5fb1bfd61380390614c31811cb2e2f178289f
wangpan <763687721@qq.com>
2020-11-08 20:32:38 +0800

    注释从git配置server服务器fetch拿取文件

---
+++ service-config/src/main/java/com/panda/ConfigServiceApplication.java
@@ -0,0 +1,16 @@
+package com.panda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServiceApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServiceApplication.class, args);
+		System.out.println("=============Config-Service已经启动================");
+	}
+	 
+}
