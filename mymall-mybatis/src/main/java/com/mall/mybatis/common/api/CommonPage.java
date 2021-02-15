6f42a58073331f74af7434369fd28f9eedffc3cf
wangpan1 <wangpan1@acca.com.cn>
2021-02-15 14:59:32 +0800

    增加操作mysql，springboot集成mybatis,自动生成entity和mapper.xml文件

---
+++ mymall-mybatis/src/main/java/com/mall/mybatis/common/api/CommonPage.java
@@ -0,0 +1,78 @@
+package com.mall.mybatis.common.api;
+
+import com.github.pagehelper.PageInfo;
+
+import java.util.List;
+
+/**
+  * @author Administrator
+  * @create 2021-02-14
+  * @desc 分页数据封装类
+**/
+public class CommonPage<T> {
+
+    /**第几页**/
+    private Integer pageNum;
+    /**每一页的条数 10 , 20 , 50**/
+    private Integer pageSize;
+    /**总页数 **/
+    private Integer totalPage;
+    /**总条数**/
+    private Long total;
+    /**对象记录list**/
+    private List<T> list;
+
+    /**
+     * 将PageHelper分页后的list转为分页信息
+     */
+    public static <T> CommonPage<T> restPage(List<T> list) {
+        CommonPage<T> result = new CommonPage<T>();
+        PageInfo<T> pageInfo = new PageInfo<T>(list);
+        result.setTotalPage(pageInfo.getPages());
+        result.setPageNum(pageInfo.getPageNum());
+        result.setPageSize(pageInfo.getPageSize());
+        result.setTotal(pageInfo.getTotal());
+        result.setList(pageInfo.getList());
+        return result;
+    }
+
+    public Integer getPageNum() {
+        return pageNum;
+    }
+
+    public void setPageNum(Integer pageNum) {
+        this.pageNum = pageNum;
+    }
+
+    public Integer getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(Integer pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public Integer getTotalPage() {
+        return totalPage;
+    }
+
+    public void setTotalPage(Integer totalPage) {
+        this.totalPage = totalPage;
+    }
+
+    public List<T> getList() {
+        return list;
+    }
+
+    public void setList(List<T> list) {
+        this.list = list;
+    }
+
+    public Long getTotal() {
+        return total;
+    }
+
+    public void setTotal(Long total) {
+        this.total = total;
+    }
+}
