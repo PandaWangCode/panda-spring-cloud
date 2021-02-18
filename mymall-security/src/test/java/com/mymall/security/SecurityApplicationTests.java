782793557aea7c399918bad2c676cf60493341e6
wangpan1 <wangpan1@acca.com.cn>
2021-02-18 23:55:33 +0800

    补充文档

--- mymall-security/src/test/java/com/mymall/security/SecurityApplicationTests.java
+++ mymall-security/src/test/java/com/mymall/security/SecurityApplicationTests.java
@@ -1,6 +1,7 @@
 package com.mymall.security;
 
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
